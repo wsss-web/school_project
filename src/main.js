376dfa9efe27f5bcc1e5a049cad7acedfd7c121c
wsss-web <1121842729@qq.com>
2020-06-20 19:04:53 +0800

    登录页面 6-20更新

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/vant.js'
-require('../node_modules/vux/src/styles/reset.less')
+import tools from '../tools.js'
+
+Vue.prototype.tools = tools
+// require('../node_modules/vux/src/styles/reset.less')
 
 Vue.config.productionTip = false
 
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,32 +1,13 @@
 <template>
     <div>
-        这是首页
-    <tabbar>
-      <tabbar-item>
-        <img slot="icon" src="">
-        <span slot="label">Wechat</span>
-      </tabbar-item>
-      <tabbar-item show-dot>
-        <img slot="icon" src="">
-        <span slot="label">Message</span>
-      </tabbar-item>
-      <tabbar-item selected link="/component/demo">
-        <img slot="icon" src="">
-        <span slot="label">Explore</span>
-      </tabbar-item>
-      <tabbar-item badge="2">
-        <img slot="icon" src="">
-        <span slot="label">News</span>
-      </tabbar-item>
-    </tabbar>
+      这是首页
     </div>
 </template>
 <script>
-import { Tabbar, TabbarItem } from 'vux'
+
 export default {
   components: {
-    Tabbar,
-    TabbarItem
+    
   }
 }
 </script>
